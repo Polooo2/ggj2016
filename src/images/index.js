5f918419f2907bb653f58f07e3ca098017fcc11f
Johannes Stein <johannesstein@freeze-dev.com>
2016-01-29 23:13:30 +0100

    Fix image index

--- src/images/index.js
+++ src/images/index.js
@@ -4,6 +4,6 @@ var req = require.context('./', false, /\.(png|jpg|gif|js)$/);
 module.exports = req.keys().reduce(function(obj, name) {
   var moduleName = path.basename(name).split(path.extname(name))[0];
 
-  obj[moduleName] = (process.env.BROWSER && path.extname(name) !== '.js') ? req(name) : null;
+  obj[moduleName] = req(name);
   return obj;
 }, {});
