acb48501419df211cfe5bf9ad9ff470a0c5b7b33
Johannes Stein <johannesstein@freeze-dev.com>
2016-01-29 22:36:06 +0100

    Much progress bar. So wow!

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -7,11 +7,12 @@ class Game extends Component {
   render() {
     return (
       <Scene name="game">
-        <ProgressBar progress={100} />
+        <ProgressBar className="belief" progress={100} />
         <Button className="button-build" onClick={this.onBuild}>Defenses</Button>
         <Button className="button-quests" onClick={this.onQuests}>Quests</Button>
         <Button className="button-upgrades" onClick={this.onUpgrades}>Upgrades</Button>
         <Button className="button-help" onClick={this.onHelp}>Help</Button>
+        <ProgressBar className="time" progress={100} />
       </Scene>
     );
   }
