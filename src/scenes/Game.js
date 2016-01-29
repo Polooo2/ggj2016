3b300bb19760706bb419119e23ac804b309d13bb
Dawid Pach <polooo2@hotmail.com>
2016-01-30 00:18:28 +0100

    dynamically add answers from decisionNodes to game screen

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -4,20 +4,29 @@ import { decisionNodes } from 'data';
 import Scene from './Scene';
 import { BackgroundImage, Button, ProgressBar, Text } from 'components';
 
-console.log('test', decisionNodes);
 
 class Game extends Component {
+  getAvailableDecisions = () => {
+    // TODO add believe
+
+    var availableDecisions = decisionNodes.filter(elem => elem.min < 20 < elem.max);
+    return availableDecisions[Math.floor(Math.random()*availableDecisions.length)];
+  };
+  selDecision = () => {
+    alert(1)
+  };
   render() {
+    const decisions = this.getAvailableDecisions().selection.map((dec, i) => {
+      return <Button className={"button-" + (i)} onClick={() => this.selDecision(dec.effect)}>{dec.text}</Button>
+    });
+
     return (
       <Scene name="game">
         <BackgroundImage src={background} />
         <ProgressBar className="belief" progress={100} />
         <div className="text-container">
           <Button className="text-area">{decisionNodes[0].intro}</Button>
-          <Button className="button-1" onClick={this.onBuild}>{decisionNodes[0].selection[0].text}</Button>
-          <Button className="button-2" onClick={this.onQuests}>{decisionNodes[0].selection[1].text}</Button>
-          <Button className="button-3" onClick={this.onUpgrades}>{decisionNodes[0].selection[2].text}</Button>
-          <Button className="button-4" onClick={this.onUpgrades}>{decisionNodes[0].selection[2].text}</Button>
+          {decisions}
           <ProgressBar className="time" progress={100} />
         </div>
       </Scene>
