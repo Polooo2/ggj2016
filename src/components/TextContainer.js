2bec42eebabaafc45b056aef0408b8a4ad4b5c5d
Johannes Stein <johannesstein@freeze-dev.com>
2016-01-30 10:15:17 +0100

    TextContainer

---
+++ src/components/TextContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TextContainer extends Component {
+  render() {
+    if (this.props.selectionVisible) {
+
+    } else {
+
+    }
+  }
+}
+
+export default TextContainer;
