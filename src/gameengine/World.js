86e71f50dfc0015b0cf0f35575dd1aa7f4ad735b
Johannes Stein <johannesstein@freeze-dev.com>
2016-01-30 11:33:59 +0100

    World component draft

---
+++ src/gameengine/World.js
@@ -0,0 +1,35 @@
+import React, { Component, PropTypes } from 'react';
+import EventMap from 'eventmap';
+
+const eventMap = new EventMap();
+
+class World extends Component {
+  static propTypes = {
+    onChange: PropTypes.func,
+  };
+
+  static defaultProps = {
+    onChange: function() {},
+  }
+
+  componentDidMount() {
+    eventMap.on('change', (key, value) => {
+      this.props.onChange(key, value);
+    });
+  }
+
+  render() {
+    return null;
+  }
+}
+
+World.values = {};
+
+World.set = (key, value) => {
+  eventMap.trigger('change', key, value);
+  World.values[key] = value;
+};
+
+World.get = key => World.values[key];
+
+export default World;
