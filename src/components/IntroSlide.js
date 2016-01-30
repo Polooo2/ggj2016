acf477ecf96b9d53fdf01f3c20631fcb8eb1fb5c
Johannes Stein <johannesstein@freeze-dev.com>
2016-01-30 16:00:13 +0100

    Fix IntroSlide

--- src/components/IntroSlide.js
+++ src/components/IntroSlide.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
 import Button from './Button';
+import Typewriter from 'react-typewriter';
 
-class IntroSlide {
+class IntroSlide extends Component {
+  static defaultProps = {
+    alignImage: 'left',
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      buttonTyping: false,
+    };
+  }
+
+  onTypingEnd = () => {
+    this.setState({
+      buttonTyping: true,
+    });
+  };
+
   render() {
+    let button = null;
+
+    if (this.state.buttonTyping) {
+      button = (
+        <Typewriter typing={1}>
+          <Button onClick={this.props.onClick}>1. {this.props.button}</Button>
+        </Typewriter>
+      );
+    }
+
     return (
       <div className={`intro-slide {this.props.className}`}>
-        <div className="intro-slide text">{this.props.text}</div>
-        <Button onClick={this.props.onClick}>1. {this.props.button}</Button>
+        <img src={this.props.image} border={0} width={320} height={240} style={{ float: this.props.alignImage, marginRight: 16 }} />
+        <div className="intro-slide-text">
+          <Typewriter typing={~~this.props.isTyping} onTypingEnd={this.onTypingEnd}>{this.props.text}</Typewriter>
+        </div>
+        {button}
       </div>
     );
   }
